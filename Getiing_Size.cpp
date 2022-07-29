a10e3d41329339cc10a91e99358e40c8595b0718
Hasmukhsingh1999 <94060756+Hasmukhsingh1999@users.noreply.github.com>
2022-07-29 12:58:12 +0530

    Delete Getiing_Size.cpp

--- Getiing_Size.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main(){
-    int a = 12 , b =34;
-    float c = 2.3;
-    // integer take 4 bytes in c++
-    std::cout<<"Size of int a :"<<sizeof(a);
-    std::cout<<"\nSize of int b : "<<sizeof(b);
-    std::cout<<"\n"<<sizeof(c);
-}
